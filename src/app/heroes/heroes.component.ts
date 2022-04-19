a83ea0cbd12879f18cc9e79f6f890b068dc6c787
PositiveNature <positive.nature.creative@gmail.com>
2022-04-20 01:28:03 +0300

    Display a List, add default selected onInit, add else statement for *ngIf

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
-  hero: Hero = { id: 1, name: 'Dr. Alan Watkins' }
-  constructor() { }
 
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  constructor() { }
+  
   ngOnInit(): void {
+    // select first element
+    this.selectedHero = HEROES[0]
+  }
+
+  onSelect(hero: Hero): void {
+    console.log(`clicked ${hero.name}`)
+    console.log(this.selectedHero)
+    if (this.selectedHero == hero) {
+      this.selectedHero = undefined;
+    } else {
+      this.selectedHero = hero;
+    }
   }
 }
