bcc5f365ad4d6cd2c47447dcad9b295bf0969f2b
PositiveNature <positive.nature.creative@gmail.com>
2022-04-25 13:08:52 +0300

    routable HeroDetailsComponent

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,4 +16,10 @@ export class HeroService {
     this.messagesService.add('HeroService: Fetched heroes')
     return heroes;
   }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messagesService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
