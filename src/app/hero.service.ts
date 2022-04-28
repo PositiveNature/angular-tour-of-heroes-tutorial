8aa12416731ff79a0dec41da6b6f4ca2afd60d48
PositiveNature <positive.nature.creative@gmail.com>
2022-04-28 13:39:33 +0300

    update HeroService.getHeroes to use HttpClient to get heroes;/ add  in memory database and service to mimic the http requests to a db server;/ add private log method to HeroService and refactor code  to use the private log method when logging messages

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,25 +1,55 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor(private messagesService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService,
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messagesService.add('HeroService: Fetched heroes')
-    return heroes;
+    // const heroes = of(HEROES);
+    // this.messagesService.add('HeroService: Fetched heroes')
+    this.log('Fetched heroes')
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
+    // return heroes;
   }
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find(h => h.id === id)!;
-    this.messagesService.add(`HeroService: fetched hero id=${id}`);
+    // this.messagesService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`fetched hero id=${id}`);
     return of(hero);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T)
+  {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    }
+  }
+
+  private heroesUrl = 'api/heroes'
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
